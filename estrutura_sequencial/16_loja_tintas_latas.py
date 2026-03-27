6f8c9ded3b0ad75cd77114a45347f5f1123c5728
fabiogsilva <fabiogsilva@disroot.org>
2026-03-27 00:56:11 -0300

    alteração estrural

--- estrutura_sequencial/16_loja_tintas_latas.py
+++ estrutura_sequencial/16_loja_tintas_latas.py
@@ -2,6 +2,9 @@
 #Date: /03/2026
 """
 Exercício 16: Loja de Tintas (Apenas Latas)
-Enunciado: Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere a cobertura de 1 litro para 3 metros quadrados. A tinta é vendida em latas de 18 litros (R$ 80,00). Informe quantidades e preço total.
+Enunciado: Faça um programa para uma loja de tintas.
+O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. 
+Considere a cobertura de 1 litro para 3 metros quadrados. 
+A tinta é vendida em latas de 18 litros (R$ 80,00). Informe quantidades e preço total.
 Link: https://exercicios.dunossauro.com/01_estrutura_sequencial/#__tabbed_1_16
 """
